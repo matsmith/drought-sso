146daef4a6ad2f1d791333912388a5a49f4dcebd
Mathew Smith <msmit205@its.jnj.com>
2016-01-07 14:55:34 -0500

    Still trying to get the services working by avoiding them.

--- controllers/Home.js
+++ controllers/Home.js
@@ -8,23 +8,23 @@ module.exports = BaseController.extend({
 		var self = this;
 		this.content.htmlClass = 'front not-logged-in page-home no-sidebars fluid-width locale-en_us home-page-not-logged-in';
 		// Transform jQuery not-real promise into real promise
-		Promise.all([
-			self.service.presentation.getBy({ page: 'portal', type: 'navigation' }),
-			self.service.companyConfig.getCompanyConfig(['ProductRenaming'])
-		]).then(function(serviceResults){
-			self.getNav(serviceResults[0]);
-			self.getContent(serviceResults[1]);
-		}).then(undefined, function(err){
-			var exception = JSON.parse(err.responseText);
-			console.log('Promise Error Type: ', exception.error);
-			console.log('Promise Error Description: ', exception.error_description);
-		}).done(function(success, error){
-			new View(res, 'home').render( self.content, { navigation: '_nav' } );
-		});
+		// Promise.all([
+		// 	self.service.presentation.getBy({ page: 'portal', type: 'navigation' }),
+		// 	self.service.companyConfig.getCompanyConfig(['ProductRenaming'])
+		// ]).then(function(serviceResults){
+		// 	self.getNav(serviceResults[0]);
+		// 	self.getContent(serviceResults[1]);
+		// }).then(undefined, function(err){
+		// 	var exception = JSON.parse(err.responseText);
+		// 	console.log('Promise Error Type: ', exception.error);
+		// 	console.log('Promise Error Description: ', exception.error_description);
+		// }).done(function(success, error){
+		// 	new View(res, 'home').render( self.content, { navigation: '_nav' } );
+		// });
 	},
 	getContent: function(data) {
 		var self = this;
-		this.content.programName = data.model.features.ProductRenaming.bingeLongName;
+		this.content.programName = "Mat App"; //data.model.features.ProductRenaming.bingeLongName;
 	},
 	getNav: function(data) {
 		var self = this;
@@ -37,8 +37,8 @@ module.exports = BaseController.extend({
 					primaryNav.push(block);
 				}
 			});
-			self.content.nav = primaryNav;
-			self.content.subNav = subNav;
+			self.content.nav = []; //primaryNav;
+			self.content.subNav = []; //subNav;
 		});
 	}
 });
